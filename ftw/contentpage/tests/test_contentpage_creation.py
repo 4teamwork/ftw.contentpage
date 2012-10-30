a52d359f17b9459570eb788a5780757356f9780e
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-10-30 16:39:30 +0100

    add tests for simplelayout integration

--- ftw/contentpage/tests/test_contentpage_creation.py
+++ ftw/contentpage/tests/test_contentpage_creation.py
@@ -1,5 +1,9 @@
 from ftw.contentpage.testing import FTW_CONTENTPAGE_INTEGRATION_TESTING
 from unittest2 import TestCase
+from simplelayout.base.interfaces import ISimpleLayoutCapable
+from plone.testing.z2 import Browser
+from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
+import transaction
 
 
 class TestContentPageCreation(TestCase):
@@ -11,7 +15,33 @@ class TestContentPageCreation(TestCase):
         self.portal = self.layer['portal']
         self.portal_url = self.portal.portal_url()
 
+        # Browser setup
+        self.browser = Browser(self.layer['app'])
+        self.browser.handleErrors = False
+
+    def _auth(self):
+        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
+                TEST_USER_NAME, TEST_USER_PASSWORD, ))
+
     def test_fti(self):
         self.assertIn('ContentPage', self.portal.portal_types.objectIds())
 
-    
+    def test_creation(self):
+        _id = self.portal.invokeFactory('ContentPage', 'contentpage')
+        self.assertIn(_id, self.portal.objectIds())
+
+    def test_simplelayout_integration(self):
+        contentpage = self.portal.get(
+            self.portal.invokeFactory('ContentPage', 'contentpage'))
+        ISimpleLayoutCapable.providedBy(contentpage)
+
+    def test_simplelayout_view(self):
+        _id = self.portal.invokeFactory('ContentPage', 'contentpage')
+        transaction.commit()
+        self._auth()
+        self.browser.open(self.portal_url + '/' + _id)
+        self.assertIn('simplelayout-container', self.browser.contents)
+
+
+
+
