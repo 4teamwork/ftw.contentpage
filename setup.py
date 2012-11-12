f300b649f4eb0e8ff80ba2a01fee1ba32c9a73c7
Jonas Baumann <jone@jone.ch>
2012-11-12 13:57:25 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -14,23 +14,27 @@ setup(name='ftw.contentpage',
       description="Contentpage based on Simplelayout for for web/intranet",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+
+      keywords='ftw contentpage',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://plone.org/products/ftw.contentpage/',
+      url='https://github.com/4teamwork/ftw.contentpage',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       install_requires=[
           'setuptools',
           'simplelayout.base',
@@ -38,6 +42,7 @@ setup(name='ftw.contentpage',
           'archetypes.schemaextender',
           'ftw.geo',
       ],
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
