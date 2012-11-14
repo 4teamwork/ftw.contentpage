0d208ffebcc07f5a2ca48f3678ff31d6ef95db62
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-11-14 19:12:40 +0100

    add gallery view

---
+++ ftw/contentpage/browser/listingblock_gallery_view.py
@@ -0,0 +1,22 @@
+from Products.Five.browser import BrowserView
+from plone.app.imaging.utils import getAllowedSizes
+
+
+class ListingBlockGalleryView(BrowserView):
+    """Block representation of ListingBlock"""
+
+    @property
+    def _build_query(self):
+        query = {}
+        query['portal_type'] = ['Image']
+        return query
+
+    def get_images(self):
+        return self.context.getFolderContents(
+            contentFilter=self._build_query,
+            full_objects=True)
+
+    def _get_box_boundaries(self):
+        sizes = getAllowedSizes()
+        # Fallback is a plone default scale -> mini
+        return sizes.get('listingblock_gallery', 'mini')
--- ftw/contentpage/tests/test_listingblockviews.py
+++ ftw/contentpage/tests/test_listingblockviews.py
@@ -1,18 +1,21 @@
 from ftw.contentpage.testing import FTW_CONTENTPAGE_FUNCTIONAL_TESTING
-from unittest2 import TestCase
-from plone.testing.z2 import Browser
+from plone.app.imaging.utils import getAllowedSizes
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
-from zope.component import queryMultiAdapter
+from plone.testing.z2 import Browser
 from StringIO import StringIO
+from unittest2 import TestCase
+from zope.component import queryMultiAdapter
+import os
 import transaction
 
 
-class TestListingBlockCreation(TestCase):
+
+class TestListingBlockViews(TestCase):
 
     layer = FTW_CONTENTPAGE_FUNCTIONAL_TESTING
 
     def setUp(self):
-        super(TestListingBlockCreation, self).setUp()
+        super(TestListingBlockViews, self).setUp()
         self.portal = self.layer['portal']
         self.portal_url = self.portal.portal_url()
 
@@ -38,6 +41,18 @@ class TestListingBlockCreation(TestCase):
         transaction.commit()
         return listingblock
 
+    def test_listing_view_registration(self):
+        listingblock = self._create_listingblock()
+        view = queryMultiAdapter((listingblock, listingblock.REQUEST),
+                                 name="block_view")
+        self.assertNotEquals(view, None)
+
+    def test_gallery_view_registration(self):
+        listingblock = self._create_listingblock()
+        view = queryMultiAdapter((listingblock, listingblock.REQUEST),
+                                 name="block_view-gallery")
+        self.assertNotEquals(view, None)
+
     def test_listingblock_view(self):
         listingblock = self._create_listingblock()
         self._auth()
@@ -61,8 +76,50 @@ class TestListingBlockCreation(TestCase):
         self.assertIn("Dummy PDF", view.render_table())
         self.assertIn("pdf.png", view.render_table())
 
+    def test_scale_installed(self):
+        listingblock = self._create_listingblock()
+        _image = listingblock.get(
+            listingblock.invokeFactory('Image', 'image'))
+        scales = _image.restrictedTraverse('@@images')
+        self.assertIn('listingblock_gallery', scales.getAvailableSizes())
+
+    def test_gallery_get_box_boundaries(self):
+        listingblock = self._create_listingblock()
+        view = queryMultiAdapter((listingblock, listingblock.REQUEST),
+                         name='block_view-gallery')
+        self.assertEquals(getAllowedSizes()['listingblock_gallery'],
+            view._get_box_boundaries())
+
+    def test_gallery_get_images(self):
+        listingblock = self._create_listingblock()
+        _image = (listingblock.get(
+            listingblock.invokeFactory('Image', 'image')))
+        listingblock.invokeFactory('File', 'file')
+        view = queryMultiAdapter((listingblock, listingblock.REQUEST),
+                         name='block_view-gallery')
+        #Only return images
+        self.assertEquals([_image], view.get_images())
+
+    def test_gallery_view(self):
+        listingblock = self._create_listingblock()
+        _image = listingblock.get(
+            listingblock.invokeFactory('Image', 'image'))
+
+        dummy = open("%s/dummy.png" % os.path.split(__file__)[0], 'r')
+        dummy.seek(0)
+        _image.setImage(dummy)
+        _image.setTitle('Dummy Image')
+        _image.processForm()
+
+        view = queryMultiAdapter((listingblock, listingblock.REQUEST),
+                                 name='block_view-gallery')
+        self.assertIn("<img src=\"http://nohost/plone/contentpage/"
+            "listingblock/image/@@images",
+                      view.index())
+        self.assertIn("title=\"Dummy Image\"", view.index())
+
     def tearDown(self):
-        super(TestListingBlockCreation, self).tearDown()
+        super(TestListingBlockViews, self).tearDown()
         portal = self.layer['portal']
         portal.manage_delObjects(['contentpage'])
 
