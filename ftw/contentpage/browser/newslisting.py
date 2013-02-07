810ea569d87c3eb0d6c29b8d74d5351c4f604f33
Julian Infanger <j.infanger@4teamwork.ch>
2013-02-07 15:30:32 +0100

    Removed searchform from newslisting.

--- ftw/contentpage/browser/newslisting.py
+++ ftw/contentpage/browser/newslisting.py
@@ -1,5 +1,4 @@
 from zope.publisher.browser import BrowserView
-import DateTime
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -13,10 +12,6 @@ class NewsListing(BrowserView):
         if self.__name__ == 'news_rss_listing':
             return self.template_rss()
         return self.template()
-
-    def get_today(self, plus=0, minus=0):
-        date = DateTime.DateTime() + plus - minus
-        return date.Date()
 
     def get_creator(self, item):
         memberid = item.Creator
@@ -34,24 +29,6 @@ class NewsListing(BrowserView):
         query['portal_type'] = 'News'
         query['sort_on'] = 'effective'
         query['sort_order'] = 'reverse'
-
-        end = self.request.form.get('end', '')
-        if end:
-            end = DateTime.DateTime(end).Date()
-        start = self.request.form.get('start', '')
-        if start:
-            start = DateTime.DateTime(start).Date()
-            if end:
-                query['effective'] = {'range': 'min:max',
-                                      'query': (start, end)}
-            else:
-                query['effective'] = {'range': 'min', 'query': start}
-        elif end:
-            query['effective'] = {'range': 'max', 'query': end}
-        else:
-            query['effective'] = {'range': 'min:max',
-                                  'query': (self.get_today(minus=30),
-                                            self.get_today())}
 
         if ct == 'Topic':
             return context.queryCatalog()
@@ -73,10 +50,6 @@ class NewsListing(BrowserView):
             'image',
             width=200,
             height=200).tag(**{'class': 'tileImage'})
-
-    def news_result(self):
-        """Returns Newslisting"""
-        return self.result_template()
 
 
 class NewsArchiveListing(NewsListing):
