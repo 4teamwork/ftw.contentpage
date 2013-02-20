e5377d034e18b5c9605f094ccb9c4e7b1bdb7af3
Thomas Buchberger <t.buchberger@4teamwork.ch>
2013-02-20 14:33:00 +0100

    Fixed tests for subject vocabulary.

--- ftw/contentpage/tests/test_vocabularies.py
+++ ftw/contentpage/tests/test_vocabularies.py
@@ -1,5 +1,8 @@
 from ftw.testing import MockTestCase
 from ftw.contentpage.vocabularies import SubjectVocabulary
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from mocker import ANY
+
 
 class TestVocabulary(MockTestCase):
 
@@ -8,7 +11,10 @@ class TestVocabulary(MockTestCase):
         self.context = self.stub()
         cat = self.stub()
         self.mock_tool(cat, 'portal_catalog')
-        self.expect(cat.uniqueValuesFor("Subject")).result([['a'], ['b'], ['c']])
+        self.expect(cat.uniqueValuesFor("Subject")).result(['Spam', 'and', 'eggs'])
+        normalizer = self.stub()
+        self.mock_utility(normalizer, IIDNormalizer)
+        self.expect(normalizer.normalize(ANY)).call(lambda term: term)
         self.replay()
 
     def test_vocabulary(self):
@@ -17,6 +23,6 @@ class TestVocabulary(MockTestCase):
         item_list = []
         for item in vocab:
             item_list.append(item.title)
-        self.assertEqual(item_list[0], 'a')
-        self.assertEqual(item_list[1], 'b')
-        self.assertEqual(item_list[2], 'c')
+        self.assertEqual(item_list[0], 'Spam')
+        self.assertEqual(item_list[1], 'and')
+        self.assertEqual(item_list[2], 'eggs')
