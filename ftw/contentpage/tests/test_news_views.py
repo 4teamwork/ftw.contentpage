b2b6164e1995ed18eae83319198408c2ce442854
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-03-18 19:47:05 +0100

    Handle archive on news listing view

--- ftw/contentpage/tests/test_news_views.py
+++ ftw/contentpage/tests/test_news_views.py
@@ -1,9 +1,36 @@
-import unittest2 as unittest
+from DateTime import DateTime
+from ftw.contentpage.browser.newslisting import extend_query_by_date
 from ftw.contentpage.testing import FTW_CONTENTPAGE_FUNCTIONAL_TESTING
-import transaction
 from plone.testing.z2 import Browser
-import os
 import datetime
+import os
+import transaction
+import unittest2 as unittest
+
+
+class TestExtendQueryByDate(unittest.TestCase):
+
+    def test_extend_query_by_date(self):
+        query = {}
+        extend_query_by_date(query, '2012/12/01')
+        self.assertEquals(query,
+            {'effective':
+                {'query': (DateTime('2012/12/01').earliestTime(),
+                           DateTime('2012/12/31').latestTime()),
+                 'range': 'minmax'}})
+
+        extend_query_by_date(query, '2013/02/01')
+        self.assertEquals(query,
+            {'effective':
+                {'query': (DateTime('2013/02/01').earliestTime(),
+                           DateTime('2013/02/28').latestTime()),
+                 'range': 'minmax'}})
+
+        # fallback
+        query = {}
+        extend_query_by_date(query, 'not a date')
+        self.assertEquals(query, {})
+
 
 class TestNewsViews(unittest.TestCase):
 
@@ -19,11 +46,17 @@ class TestNewsViews(unittest.TestCase):
         self.news = self.newsfolder.get(self.newsfolder.invokeFactory(
                 'News', 'news', title="My News", image=image_))
         self.news1 = self.newsfolder.get(self.newsfolder.invokeFactory(
-                'News', 'news1', title="My News1", image=image_, effectiveDate=(datetime.datetime.now() - datetime.timedelta(7))))
+                'News', 'news1', title="My News1", image=image_,
+                effectiveDate=(
+                    datetime.datetime.now() - datetime.timedelta(7))))
         self.news2 = self.newsfolder.get(self.newsfolder.invokeFactory(
-                'News', 'news2', title="My News2", image=image_, effectiveDate=(datetime.datetime.now() - datetime.timedelta(14))))
+                'News', 'news2', title="My News2", image=image_,
+                effectiveDate=(
+                    datetime.datetime.now() - datetime.timedelta(14))))
         self.news3 = self.newsfolder.get(self.newsfolder.invokeFactory(
-                'News', 'news3', title="My News3", image=image_, effectiveDate=(datetime.datetime.now() - datetime.timedelta(21))))
+                'News', 'news3', title="My News3", image=image_,
+                effectiveDate=(
+                    datetime.datetime.now() - datetime.timedelta(21))))
 
         transaction.commit()
         self.browser = Browser(self.layer['app'])
