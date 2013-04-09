715e619a67d7e2c399952b878c3065b1f90a855d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-04-09 17:15:58 +0200

    events.py pep8 format

--- ftw/contentpage/content/event.py
+++ ftw/contentpage/content/event.py
@@ -18,7 +18,8 @@ else:
 
 
 EventSchema = ContentPageSchema.copy() + image_schema.copy() + atapi.Schema((
-    atapi.DateTimeField('startDate',
+    atapi.DateTimeField(
+        'startDate',
         required=True,
         searchable=False,
         accessor='start',
@@ -27,10 +28,11 @@ EventSchema = ContentPageSchema.copy() + image_schema.copy() + atapi.Schema((
             label=_(u'label_event_start', default=u'Event Starts'),
             description=_(u'help_start',
                           default=u"Date and Time, when the event begins."),
-            ),
         ),
+    ),
 
-    atapi.DateTimeField('endDate',
+    atapi.DateTimeField(
+        'endDate',
         required=True,
         searchable=False,
         accessor='end',
@@ -39,26 +41,27 @@ EventSchema = ContentPageSchema.copy() + image_schema.copy() + atapi.Schema((
             label=_(u'label_event_end', default=u'Event Ends'),
             description=_(u'help_end',
                           default=u"Date and Time, when the event ends."),
-            ),
         ),
+    ),
 
-    atapi.BooleanField('wholeDay',
+    atapi.BooleanField(
+        'wholeDay',
         default=False,
         languageIndependent=True,
         widget=atapi.BooleanWidget(
             label=_(u'label_whole_day_event', u'Whole day event'),
             description=_(u'help_whole_day', default=u"Event lasts whole day"),
-            ),
         ),
-    atapi.StringField('location',
-                      searchable=True,
-                      widget=atapi.StringWidget(
-                label=_(u'label_event_location', default=u'Event Location'),
-                description=_(u'help_event_location', default=u""),
-                ),
-                ),
-
-    ))
+    ),
+    atapi.StringField(
+        'location',
+        searchable=True,
+        widget=atapi.StringWidget(
+            label=_(u'label_event_location', default=u'Event Location'),
+            description=_(u'help_event_location', default=u""),
+        ),
+    ),
+))
 
 finalizeATCTSchema(EventSchema)
 # finalizeATCTSchema moves 'location' into 'categories', we move it back:
@@ -80,6 +83,7 @@ class Event(ContentPage):
             end = DT2dt(self.end())
             end_date = end.date().strftime('%d.%m.%Y')
             end_time = end.time().strftime('%H:%M')
+            # XXX not readable us %(dict)s
             if start_date == end_date:
                 return start_date + ' ' + start_time + ' - ' + end_time
             else:
