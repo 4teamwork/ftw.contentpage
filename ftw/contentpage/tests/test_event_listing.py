ae2bed7213c123d88a211989e03a71c050e7107d
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-04-09 17:50:09 +0200

    pep8

--- ftw/contentpage/tests/test_event_listing.py
+++ ftw/contentpage/tests/test_event_listing.py
@@ -1,14 +1,10 @@
-import unittest2 as unittest
-from ftw.contentpage.testing import FTW_CONTENTPAGE_FUNCTIONAL_TESTING
-import transaction
-from simplelayout.base.interfaces import ISimpleLayoutCapable
-from plone.testing.z2 import Browser
-from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
-from StringIO import StringIO
-import os
 from DateTime import DateTime
-from ftw.contentpage.browser.eventlisting import EventListing
+from ftw.contentpage.testing import FTW_CONTENTPAGE_FUNCTIONAL_TESTING
+from plone.testing.z2 import Browser
 from pyquery import PyQuery
+import os
+import transaction
+import unittest2 as unittest
 
 
 class TestEventListing(unittest.TestCase):
@@ -17,35 +13,42 @@ class TestEventListing(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.eventfolder = self.portal.get(self.portal.invokeFactory('EventFolder', 'eventfolder'))
+        self.eventfolder = self.portal.get(
+          self.portal.invokeFactory('EventFolder', 'eventfolder'))
         self.eventfolder.invokeFactory('EventPage', 'event1', title="Event1",
-                                       startDate=DateTime(2013, 05, 20, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 05, 20, 10, 00),
                                        endDate=DateTime(2013, 05, 20, 11, 00),
-                                       location = "Bern")
+                                       location="Bern")
 
         self.eventfolder.invokeFactory('EventPage', 'event2', title="Event2",
-                                       startDate=DateTime(2013, 05, 22, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 05, 22, 10, 00),
                                        endDate=DateTime(2013, 05, 22, 11, 00),
                                        location="Bern")
         file_ = open("%s/dummy.png" % os.path.split(__file__)[0], 'r')
         self.eventfolder.invokeFactory('EventPage', 'event3', title="Event3",
-                                       startDate=DateTime(2013, 05, 24, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 05, 24, 10, 00),
                                        endDate=DateTime(2013, 05, 24, 11, 00),
                                        location="Berns",
                                        image=file_)
 
         self.eventfolder.invokeFactory('EventPage', 'event4', title="Event4",
-                                       startDate=DateTime(2013, 05, 26, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 05, 26, 10, 00),
                                        endDate=DateTime(2013, 05, 26, 11, 00),
                                        location="Bern")
 
         self.eventfolder.invokeFactory('EventPage', 'event5', title="Event5",
-                                       startDate=DateTime(2013, 06, 28, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 06, 28, 10, 00),
                                        endDate=DateTime(2013, 06, 28, 11, 00),
                                        location="Bern")
 
         self.eventfolder.invokeFactory('EventPage', 'event6', title="Event6",
-                                       startDate=DateTime(2013, 05, 30, 10, 00),
+                                       startDate=DateTime(
+                                           2013, 05, 30, 10, 00),
                                        endDate=DateTime(2013, 05, 30, 11, 00),
                                        location="Bern")
 
@@ -62,7 +65,7 @@ class TestEventListing(unittest.TestCase):
         view = self.eventfolder.restrictedTraverse('event_listing')
         events = view.getEvents()
         for index, event in enumerate(events):
-            if not index == len(events) -1:
+            if not index == len(events) - 1:
                 self.assertTrue(event.start < events[index + 1].start)
 
     def test_listing_archive(self):
@@ -71,7 +74,6 @@ class TestEventListing(unittest.TestCase):
         events = view.getEvents()
         self.assertEqual(len(events), 1)
         self.assertEqual(events[0].start, DateTime(2013, 06, 28, 10, 00))
-
 
     def test_has_img(self):
         view = self.eventfolder.restrictedTraverse('event_listing')
