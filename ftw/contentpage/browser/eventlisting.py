39c3b3f4c178fe185068d2467526b0296dd4b400
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-04-10 10:27:18 +0200

    Update eventlisting and newslisting. use baseclass to prevent duplicate code.

--- ftw/contentpage/browser/eventlisting.py
+++ ftw/contentpage/browser/eventlisting.py
@@ -3,60 +3,21 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
 from DateTime.interfaces import SyntaxError as dtSytaxError
+from ftw.contentpage.browser.baselisting import BaseListing
 
+class EventListing(BaseListing):
 
-class EventListing(BrowserView):
     template = ViewPageTemplateFile("eventlisting.pt")
 
-    def __call__(self):
-        return self.template()
 
-    def getEvents(self):
-        if not self.request.get('archiv'):
-            cat = getToolByName(self.context, 'portal_catalog')
+    def get_items(self):
             query = {'path': '/'.join(self.context.getPhysicalPath()),
                      'portal_type': 'EventPage',
                      'sort_on': 'start',
-                     'sort_limit': 5,
                      'start':{'query':DateTime(), 'range': 'min'}
                      }
-            events = cat.searchResults(query)
-            return events[0:5]
-        else:
-            cat = getToolByName(self.context, 'portal_catalog')
-            query = {'path': '/'.join(self.context.getPhysicalPath()),
-                     'portal_type': 'EventPage',
-                     'sort_on': 'start'
-                     }
-            extend_query_by_date(query, self.request.get('archiv'))
-            events = cat.searchResults(query)
-            return events
+            return self.search_results(query, 'start')
 
     def getDate(self, brain):
         obj = brain.getObject()
         return obj.getDate()
-
-    def has_img(self, brain):
-        obj = brain.getObject()
-        return obj.getImage()
-
-    def get_img(self, brain):
-        obj = brain.getObject()
-        scale = obj.restrictedTraverse('@@images')
-        return scale.scale(
-            'image',
-            width=100,
-            height=100).tag(**{'class': 'tileImage'})
-
-
-def extend_query_by_date(query, datestring):
-    try:
-        start = DateTime(datestring)
-    except dtSytaxError:
-        return
-    end = DateTime('%s/%s/%s' % (start.year() + start.month() / 12,
-                                 start.month() % 12 + 1, 1))
-    end = end - 1
-    query['start'] = {'query': (start.earliestTime(),
-                                    end.latestTime()),
-                          'range': 'minmax'}
