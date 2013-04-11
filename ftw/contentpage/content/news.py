3a66d58bf42b888261ef6c1812c69430a580bfc4
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-04-11 11:45:57 +0200

    News now inherits from contentpage

--- ftw/contentpage/content/news.py
+++ ftw/contentpage/content/news.py
@@ -3,10 +3,7 @@ from AccessControl import ClassSecurityInfo
 from ftw.contentpage.config import PROJECTNAME
 from zope.interface import implements
 from ftw.contentpage.interfaces import INews
-from Products.ATContentTypes.content import folder
-from simplelayout.base.interfaces import ISimpleLayoutCapable
-from ftw.contentpage.content.textblock import image_schema
-from ftw.contentpage.interfaces import ITeaser
+from ftw.contentpage.content import contentpage
 from Products.ATContentTypes.config import HAS_LINGUA_PLONE
 if HAS_LINGUA_PLONE:
     from Products.LinguaPlone.public import registerType
@@ -14,7 +11,7 @@ else:
     from Products.Archetypes.atapi import registerType
 
 
-news_schema = folder.ATFolderSchema.copy() + image_schema.copy()
+news_schema = contentpage.ContentPageSchema.copy()
 
 news_schema['effectiveDate'].required = True
 news_schema['effectiveDate'].default_method = 'getDefaultEffectiveDate'
@@ -23,23 +20,15 @@ news_schema.changeSchemataForField('expirationDate', 'default')
 news_schema.moveField('image', after='description')
 
 
-class News(folder.ATFolder):
+class News(contentpage.ContentPage):
 
-    implements(INews, ISimpleLayoutCapable, ITeaser)
+    implements(INews)
     security = ClassSecurityInfo()
 
     schema = news_schema
 
     def getDefaultEffectiveDate(self):
         return DateTime().Date()
-
-    security.declarePublic('showAddMenu')
-    def showAddMenu(self):
-        return False
-
-    security.declarePublic('show_description')
-    def show_description(self):
-        return False
 
 
 registerType(News, PROJECTNAME)
