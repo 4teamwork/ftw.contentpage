84cf21f56d6c26de76cc24157ae02aece869a0df
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-04-26 10:37:47 +0200

    Add custom date formatter for events

--- ftw/contentpage/browser/eventlisting.py
+++ ftw/contentpage/browser/eventlisting.py
@@ -1,6 +1,24 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from DateTime import DateTime
 from ftw.contentpage.browser.baselisting import BaseListing
+from Products.ATContentTypes.utils import DT2dt
+
+
+def format_date(start, end, wholeday):
+    start = DT2dt(start)
+    start_date = start.date().strftime('%d.%m.%Y')
+    start_time = start.time().strftime('%H:%M')
+    end = DT2dt(end)
+    end_date = end.date().strftime('%d.%m.%Y')
+    end_time = end.time().strftime('%H:%M')
+    if wholeday:
+        if start_date == end_date:
+            return start_date
+        return '%s - %s' % (start_date, end_date)
+    if start_date == end_date:
+        return '%s %s - %s' % (start_date, start_time, end_time)
+    return '%s %s - %s %s' % (
+          start_date, start_time, end_date, end_time)
 
 
 class EventListing(BaseListing):
@@ -15,6 +33,6 @@ class EventListing(BaseListing):
                  }
         return self.search_results(query, 'start')
 
-    def getDate(self, brain):
+    def get_date(self, brain):
         obj = brain.getObject()
-        return obj.getDate()
+        return format_date(obj.start(), obj.end(), obj.getWholeDay())
--- ftw/contentpage/tests/test_event_date.py
+++ ftw/contentpage/tests/test_event_date.py
@@ -1,39 +1,38 @@
 from DateTime import DateTime
-from ftw.contentpage.testing import FTW_CONTENTPAGE_FUNCTIONAL_TESTING
-from plone.testing.z2 import Browser
-import transaction
+from ftw.contentpage.browser.eventlisting import format_date
 import unittest2 as unittest
 
 
 class TestEvent(unittest.TestCase):
 
-    layer = FTW_CONTENTPAGE_FUNCTIONAL_TESTING
+    def test_same_date(self):
+        start = DateTime(2013, 01, 01, 18, 00)
+        end = DateTime(2013, 01, 01, 19, 00)
+        wholeday = False
 
-    def setUp(self):
-        self.portal = self.layer['portal']
-        self.eventfolder = self.portal.get(
-            self.portal.invokeFactory('EventFolder', 'eventfolder'))
-        self.event = self.eventfolder.get(
-            self.eventfolder.invokeFactory('EventPage', 'event'))
-        transaction.commit()
-        self.browser = Browser(self.layer['app'])
-        self.browser.handleErrors = False
+        self.assertEquals(format_date(start, end, wholeday),
+            '01.01.2013 18:00 - 19:00')
 
-    def test_event_date_single_day(self):
-        self.event.startDate = DateTime(2013, 05, 30, 18, 00)
-        self.event.endDate = DateTime(2013, 05, 30, 19, 00)
-        date = self.event.getDate()
-        self.assertEqual(date, '30.05.2013 18:00 - 19:00')
+    def test_diff_date(self):
+        start = DateTime(2013, 01, 01, 18, 00)
+        end = DateTime(2013, 01, 03, 19, 00)
+        wholeday = False
 
-    def test_event_date_multiple_day(self):
-        self.event.startDate = DateTime(2013, 05, 30, 18, 00)
-        self.event.endDate = DateTime(2013, 05, 31, 19, 00)
-        date = self.event.getDate()
-        self.assertEqual(date, '30.05.2013 18:00 - 31.05.2013 19:00')
+        self.assertEquals(format_date(start, end, wholeday),
+            '01.01.2013 18:00 - 03.01.2013 19:00')
 
-    def test_event_date_whole_day(self):
-        self.event.startDate = DateTime(2013, 05, 30, 18, 00)
-        self.event.endDate = DateTime(2013, 05, 30, 19, 00)
-        self.event.wholeDay = True
-        date = self.event.getDate()
-        self.assertEqual(date, '30.05.2013')
+    def test_same_date_wholeday(self):
+        start = DateTime(2013, 01, 01)
+        end = DateTime(2013, 01, 01)
+        wholeday = True
+
+        self.assertEquals(format_date(start, end, wholeday),
+            '01.01.2013')
+
+    def test_diff_date_wholeday(self):
+        start = DateTime(2013, 01, 01)
+        end = DateTime(2013, 01, 03)
+        wholeday = True
+
+        self.assertEquals(format_date(start, end, wholeday),
+            '01.01.2013 - 03.01.2013')
