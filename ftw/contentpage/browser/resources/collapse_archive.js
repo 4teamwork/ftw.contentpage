9bebba12e72a4409befcb5941049484dd526513a
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-08-05 16:07:03 +0200

    Do not use jq in collaps_archive.js

--- ftw/contentpage/browser/resources/collapse_archive.js
+++ ftw/contentpage/browser/resources/collapse_archive.js
@@ -1,7 +1,8 @@
-jq(document).ready(function(){
-    jq('.portletArchiveListing .year span.yearnumber').click(function() {
-        jq(this).parent().toggleClass('expanded');
-        jq(this).parent().children('ul').slideToggle();
-     });
-
+jQuery(function($){
+    $(document).ready(function(){
+        $('.portletArchiveListing .year span.yearnumber').click(function() {
+            $(this).parent().toggleClass('expanded');
+            $(this).parent().children('ul').slideToggle();
+         });
+    });
 });
