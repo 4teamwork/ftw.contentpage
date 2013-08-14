534a9888d3610e966b4aa317d80320f11ab5ea88
Timon Tschanz <timon.tschanz@4teamwork.ch>
2013-08-14 14:08:29 +0200

    update faq view

--- ftw/contentpage/browser/resources/toggle_faq.js
+++ ftw/contentpage/browser/resources/toggle_faq.js
@@ -1,19 +1,18 @@
-function MeetingItemToggler(){
-        $('.faqtitle').click(function(e){
-                e.preventDefault();
-                var parentItem = $(this).closest('.FAQWrapper');
-                var meetingBody = $('#'+parentItem.attr('id') + ' .faqcontent');
+jQuery(function($){
+  $('.faqtitle').click(function(e){
+    e.preventDefault();
+    var parentItem = $(this).closest('.FAQWrapper');
+    var meetingBody = $('#'+parentItem.attr('id') + ' .faqcontent');
 
-                if (meetingBody.css('display') != 'none'){
-                        $('#'+parentItem.attr('id') + ' .sl-text-wrapper').hide('blind', 100);
-                        $('#'+parentItem.attr('id') + ' .faqtitle .toggleImage').attr('src',portal_url+'/++resource++ftw.contentpage.resources/arrow_right.png');
-                    }
-                else {
-                        $('#'+parentItem.attr('id') + ' .sl-text-wrapper').show('blind', 100);
-                        $('#'+parentItem.attr('id') + ' .faqtitle .toggleImage').attr('src',portal_url+'/++resource++ftw.contentpage.resources/arrow_down.png');
-                    }
-
-
-            }).children('.sl-actions').click(function(event) {event.stopPropagation();});
+    if (meetingBody.css('display') != 'none'){
+      $('#'+parentItem.attr('id') + ' .faqcontent').hide('blind', 100);
+      $('#'+parentItem.attr('id') + ' .faqtitle .toggleImage').attr('src',portal_url+'/++resource++ftw.contentpage.resources/arrow_right.png');
     }
-$(MeetingItemToggler);
+    else {
+      $('#'+parentItem.attr('id') + ' .faqcontent').show('blind', 100);
+      $('#'+parentItem.attr('id') + ' .faqtitle .toggleImage').attr('src',portal_url+'/++resource++ftw.contentpage.resources/arrow_down.png');
+    }
+
+
+  }).children('.sl-actions').click(function(event) {event.stopPropagation();});
+});
