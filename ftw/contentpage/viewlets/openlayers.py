917a8456e1e0d9e7ce8b2cefb8de25e8e42e96a3
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-03-25 17:56:07 +0100

    Enable openlayer resources on IContentPage if an AddressBlock is available.

--- ftw/contentpage/viewlets/openlayers.py
+++ ftw/contentpage/viewlets/openlayers.py
@@ -1,6 +1,10 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class OpenlayersViewlet(ViewletBase):
     index = ViewPageTemplateFile('openlayers.pt')
+
+    def available(self):
+        return bool(self.context.getFolderContents({
+            'portal_type': 'AddressBlock'}))
