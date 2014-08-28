e4e899c916657034f5c7c5c0805417f997e5395f
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-08-28 09:07:01 +0200

    Back to development: 1.6.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-version = '1.6.7.1'
+version = '1.6.7.2.dev0'
 mainainter = 'Mathias Leimgruber'
 
 tests_require = ['ftw.testing [splinter]',
