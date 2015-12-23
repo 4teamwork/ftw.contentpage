938c9aa2ba138fed51cc82053ecbe556a8637243
Lukas Knoepfel <shylux@gmail.com>
2015-12-23 11:44:52 +0100

    Back to development: 1.11.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-version = '1.11.7'
+version = '1.11.8.dev0'
 mainainter = 'Mathias Leimgruber'
 
 tests_require = ['ftw.testing [splinter]',
