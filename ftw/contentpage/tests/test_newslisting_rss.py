f523bf1aa8bf76fd09ae64a86f287723300c3249
Jonas Baumann <jone@jone.ch>
2019-11-12 18:25:57 +0100

    Fix rss test.

--- ftw/contentpage/tests/test_newslisting_rss.py
+++ ftw/contentpage/tests/test_newslisting_rss.py
@@ -49,12 +49,10 @@ class TestNewsRssListing(TestCase):
     @browsing
     def test_newsitem_contains_pubdate(self, browser):
         browser.open(self.newsfolder, view='news_rss_listing')
-        browser.parse_as_html()  # use HTML parser so that we have no XML namespaces.
-
         effective_date = self.news.effective()
         self.assertEqual(
             # Since w3c suggests using rfc822 for pubDate, we can simply
             # use the already implemented method for this.
             effective_date.rfc822(),
-            browser.css('rss item pubDate').first.text
+            browser.css('pubDate').first.text
         )
